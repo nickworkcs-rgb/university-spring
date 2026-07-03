8ce10f03d31ff5bb4e86d4b7f8d083c8475617e8
Nikita Malinkin <nik.malina20@gmail.com>
2026-07-03 18:15:06 +0300

    feat: добавлены сущности: Faculty, Student и Book и связи между ними

---
+++ src/main/java/ru/project/university/models/Book.java
@@ -0,0 +1,35 @@
+package ru.project.university.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "books")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = "student")
+@ToString(exclude = "student")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false, columnDefinition = "text")
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = true)
+    private Student student;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+}
